6b058282916b97373a4f83faceb41f492433a71d
SlideMovies <87663026+SlideMovies@users.noreply.github.com>
2022-08-01 11:01:00 +0500

    Remove allow CORS by default

--- convert.php
+++ convert.php
@@ -1,8 +1,4 @@
 <?php
-  // Uncomment these lines to enable CORS
-  /* header('Access-Control-Allow-Origin: *');
-  header('Access-Control-Allow-Methods: GET, POST');
-  header("Access-Control-Allow-Headers: X-Requested-With"); */
   $api_key = "my_api_key"; // Please provide your own TMDb API key.
   $m = json_decode(file_get_contents("https://api.themoviedb.org/3/tv/$_GET[id]/season/$_GET[s]?api_key=$api_key"));
   $date = $m->air_date;
