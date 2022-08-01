3ec820d5e7e05552d7633dc52574bb850407e1f4
SlideMovies <87663026+SlideMovies@users.noreply.github.com>
2022-08-01 10:49:27 +0500

    Make convert.php more precise

--- convert.php
+++ convert.php
@@ -5,6 +5,6 @@
   $year = explode("-", $date)[0];
   $y = json_decode(file_get_contents("https://api.themoviedb.org/3/tv/$_GET[id]?api_key=$api_key"));
   $title = $y->name;
-  $x = json_decode(file_get_contents(str_replace(" ", "%20", "https://api.jikan.moe/v4/anime?q=$title&start_date=$year&order_by=start_date")));
+  $x = json_decode(file_get_contents(str_replace(" ", "%20", "https://api.jikan.moe/v4/anime?q=$title&start_date=$date&order_by=start_date")));
   echo $x->data[0]->mal_id;
 ?>
