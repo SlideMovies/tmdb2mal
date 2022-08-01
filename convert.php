8b1829725da3f8ac831eb9f260d6b6837884e062
SlideMovies <87663026+SlideMovies@users.noreply.github.com>
2022-08-01 10:55:08 +0500

    Fix more precise conversions

--- convert.php
+++ convert.php
@@ -1,10 +1,16 @@
 <?php
-  $api_key = "MyAPIKey"; // Please provide your own TMDb API key.
+  // Uncomment these lines to enable CORS
+  /* header('Access-Control-Allow-Origin: *');
+  header('Access-Control-Allow-Methods: GET, POST');
+  header("Access-Control-Allow-Headers: X-Requested-With"); */
+  $api_key = "my_api_key"; // Please provide your own TMDb API key.
   $m = json_decode(file_get_contents("https://api.themoviedb.org/3/tv/$_GET[id]/season/$_GET[s]?api_key=$api_key"));
   $date = $m->air_date;
+  $month = explode("-", $date)[1];
   $year = explode("-", $date)[0];
+  $yearAndMonth = $year.'-'.$month;
   $y = json_decode(file_get_contents("https://api.themoviedb.org/3/tv/$_GET[id]?api_key=$api_key"));
   $title = $y->name;
-  $x = json_decode(file_get_contents(str_replace(" ", "%20", "https://api.jikan.moe/v4/anime?q=$title&start_date=$date&order_by=start_date")));
+  $x = json_decode(file_get_contents(str_replace(" ", "%20", "https://api.jikan.moe/v4/anime?q=$title&start_date=$yearAndMonth&order_by=start_date")));
   echo $x->data[0]->mal_id;
 ?>
